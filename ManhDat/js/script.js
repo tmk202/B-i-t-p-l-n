1ccca07abfe3bc6166cac7e674a68997c91d3abf
Dat Nguyen Dinh Manh <datndm72@wru.vn>
2019-05-20 21:42:01 +0700

    add new folder manhdat

---
+++ ManhDat/js/script.js
@@ -0,0 +1,26 @@
+$( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="dialog" title="Basic dialog">
+  <p>This is an animated dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+</div>
+ 
+<button id="opener">  </button>
